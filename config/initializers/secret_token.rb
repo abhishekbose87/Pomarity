43a1edf30409950ae28a640efc0be098e63c98fd
Abhishek Bose <abhishekbose87@gmail.com>
2013-04-12 19:31:13 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomarity::Application.config.secret_token = '9620913936be72abedc187591e2a9d15ae20519717c303059e98af4788271a5347b63b485337d000c362fb61e3c02b33eb6c5e27d4830fa0e3f882a3927b6937'
